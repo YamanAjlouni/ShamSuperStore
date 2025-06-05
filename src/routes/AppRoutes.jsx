f98c539eadaf8af4cd21b0dd8a81bf661f28e7b4
Yaman Ajlouni <yamanajlouni1@hotmail.com>
2025-06-05 16:46:01 +0300

    added my account new page but need edit in structure

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -4,6 +4,7 @@ import { Home } from '../pages/home/Home'
 import Shop from '../pages/shop/Shop'
 import CategoryDetail from '../pages/shop/categoryDetail/CategoryDetail'
 import ProductsList from '../pages/shop/productsList/ProductsList'
+import MyAccount from '../pages/myAccount/MyAccount'
 
 export const AppRoutes = () => {
   return (
@@ -12,6 +13,7 @@ export const AppRoutes = () => {
       <Route path="/shop" element={<Shop />} />
       <Route path="/shop/category/:categoryId" element={<CategoryDetail />} />
       <Route path="/shop/category/:categoryId/subcategory/:subcategoryId" element={<ProductsList />} />
+      <Route path="/myAccount" element={<MyAccount />} />
     </Routes>
   )
 }
